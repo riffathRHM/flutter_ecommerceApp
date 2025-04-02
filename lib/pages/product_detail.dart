67f19d0648ea58c764dc8885d083a503dd1b5be3
Mohamed Riffath <mohemmad.riffath@elegantmedia.lk>
2025-04-02 13:02:23 +0530

    complete product detail UI

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/widget/support_widget.dart';
 
 class ProductDetail extends StatefulWidget {
   const ProductDetail({super.key});
@@ -13,8 +14,9 @@ class _ProductDetailState extends State<ProductDetail> {
     return Scaffold(
       backgroundColor: Color(0xFFfef5f1),
       body: Container(
-        padding: EdgeInsets.only(top: 50.0, left: 20.0),
-        child: (Column(
+        padding: EdgeInsets.only(top: 50.0),
+        child: 
+          Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Stack(
@@ -23,6 +25,7 @@ class _ProductDetailState extends State<ProductDetail> {
                   Navigator.pop(context);
                 },
                 child: Container(
+                  margin: EdgeInsets.only(left: 20.0),
                     padding: EdgeInsets.all(10),
                     decoration: BoxDecoration(
                         border: Border.all(),
@@ -33,16 +36,48 @@ class _ProductDetailState extends State<ProductDetail> {
                 child: Image.asset("images/headphone2.png",height: 400
                 )
               ),
-              Container(
-                decoration: BoxDecoration(
-                  
-                ),
-                width: MediaQuery.of(context).size.width,
-              )
+              
           ]),
-           
+           Expanded(
+             child: Container(
+              padding: EdgeInsets.only(top: 20.0,left: 20.0,right: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+                  ),
+                  width: MediaQuery.of(context).size.width,child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        
+                        children: [
+                          Text("HeadPhone",style: AppWidget.boldTextFieldStyle(),),
+                          Text("\$300",
+                          style: TextStyle(
+                          color: Color(0xFFfd6f3e),
+                          fontSize: 23.0,
+                          fontWeight: FontWeight.bold))
+                        ],
+
+                      ),
+                      SizedBox(height: 20.0,),
+                      Text("Details",style: AppWidget.semiBoldTextFieldStyle(),),
+                      SizedBox(height: 10),
+                      Text("The Product is very good.It has 1 year waranty. These headphones are too good like you can also listen a person who is speaking slowely.But be aware riffath speaking loudly."),
+                      SizedBox(height: 90.0),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        decoration: BoxDecoration(color: Color(0xFFfd6f3e),borderRadius: BorderRadius.circular(10.0)),
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(child: Text("Buy Now",style: TextStyle(color: Colors.white,fontSize: 20.0,fontWeight: FontWeight.bold))),
+                      )
+                    ],
+                  ),
+                ),
+           )
           ],
-        )),
+        ),
       ),
     );
   }
